576c9af8ea7a8b5840744b52a888228045e3914d
Shehta2000 <mohamedshehta819@gmail.com>
2024-11-30 16:38:03 +0200

    Create Custom List View Item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,6 @@
 
 class AssetsData{
   static  const logo = 'assets/images/Logo.png';
+    static  const image1 = 'assets/images/Book 1 Hightligh.png';
+
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,7 +10,8 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return  const Column(
       children: [
-        CustomAppBar()
+        CustomAppBar(),
+        CustomListViewItem()
 
       ],
     );
