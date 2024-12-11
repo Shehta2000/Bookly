698e9315d495fa11e4563eced6f84840dc0a8f66
Shehta2000 <mohamedshehta819@gmail.com>
2024-12-11 15:34:19 +0200

    bestsellerListView Done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/assets.dart';
+import 'bestseller_list_view.dart';
 import 'bestseller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
@@ -11,27 +12,43 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
+    return const CustomScrollView(
+      slivers: [
+          SliverToBoxAdapter(
+             child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: CustomAppBar(),
+          ),
           FeaturedBooksListView(),
           SizedBox(
             height: 50,
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              'Best Seller',
+              style: Styles.textStyle18,
+            ),
           ),
           SizedBox(
             height: 20,
           ),
-          BestSellerListVewItem(),
+         
         ],
       ),
+          ),
+             SliverFillRemaining(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: BestSellerListView(),
+              ),
+            
+          )
+      ],
+     
     );
   }
 }
-
