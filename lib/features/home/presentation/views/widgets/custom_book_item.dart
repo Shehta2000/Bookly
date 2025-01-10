3d56fb7630933f96520c58713d6958abae657b63
Shehta2000 <mohamedshehta819@gmail.com>
2025-01-10 16:41:21 +0200

    Add Cached_Network_Image to Project

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,6 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assets.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imageUrl});
@@ -9,18 +8,18 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: DecorationImage(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),  
+        child: AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: CachedNetworkImage(
+              placeholder: (context, url) => const Center(
+                child: CircularProgressIndicator(),
+              ),
               fit: BoxFit.fill,
-              image: NetworkImage(imageUrl),
-            ),
-          ),
-        ),
+              imageUrl: imageUrl,
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            )),
       ),
     );
   }
